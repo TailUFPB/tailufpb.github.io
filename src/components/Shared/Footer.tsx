b849943f66cf24a0ace8dd6a53c9390cef9b72f9
Tiago Trindade <tiagotrindade03@gmail.com>
2024-05-12 18:26:33 -0300

    Landing Page and Footer complete

--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 
 import './styles.css';
+import FooterLogo from '../../assets/footer-logo.png';
+
+import InstagramSVG from '../../assets/socials/instagram.svg';
+import LinkedinSVG from '../../assets/socials/linkedin.svg';
+import GithubSVG from '../../assets/socials/github.svg';
 
 const Footer: React.FC = () => {
     return (
-        <footer>
-            FOOTER
-            {/* Footer content goes here */}
+        <footer className='footer-container'>
+            <div className='footer-logo'>
+                <img src={FooterLogo} alt="Footer Logo" />
+            </div>
+            <div className='footer-content'>
+                <ul className='footer-routes-elements'>
+                    <li><a href="/sobre">Sobre</a></li>
+                    <li><a href="/rotacoes">Rotações</a></li>
+                    <li><a href="/membros">Membros</a></li>
+                    <li><a href="/projetos">Projetos</a></li>
+                    <li><a href="/contato">Contato</a></li>
+                </ul>
+                <div className='footer-socials'>
+                    <a href='https://www.instagram.com/tailufpb/'>
+                        <img src={InstagramSVG} alt="Instagram" className="social-icon" />
+                    </a>
+                    <a href='https://www.linkedin.com/company/tailufpb/mycompany/'>
+                        <img src={LinkedinSVG} alt="Linkedin" className="social-icon" />
+                    </a>
+                    <a href='https://github.com/TailUFPB'>
+                        <img src={GithubSVG} alt="Github" className="social-icon" />
+                    </a>
+                </div>
+            </div>
         </footer>
     );
 };
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -3,12 +3,27 @@ import React from 'react';
 import './LandingPage.css';
 // import component1, component2 from '../../components/LandingPage/{Component}';
 import HeroLogo from '../../assets/landing-page-logo.png';
+import InstagramSVG from '../../assets/socials/instagram.svg';
+import LinkedinSVG from '../../assets/socials/linkedin.svg';
+import GithubSVG from '../../assets/socials/github.svg';
 
 const LandingPage: React.FC = () => {
     return (
         <div className='landing-page-container'>
             <h1 className="hero-text">TAIL</h1>
             <img src={HeroLogo} alt="Hero" className="hero-image" />
+            <h1 className="hero-subtitle">Technology and Artificial Intelligence League</h1>
+            <div className='landing-page-socials'>
+                <a href='https://www.instagram.com/tailufpb/'>
+                    <img src={InstagramSVG} alt="Instagram" className="social-icon" />
+                </a>
+                <a href='https://www.linkedin.com/company/tailufpb/mycompany/'>
+                    <img src={LinkedinSVG} alt="Linkedin" className="social-icon" />
+                </a>
+                <a href='https://github.com/TailUFPB'>
+                    <img src={GithubSVG} alt="Github" className="social-icon" />
+                </a>
+            </div>
         </div>
     );
 };
