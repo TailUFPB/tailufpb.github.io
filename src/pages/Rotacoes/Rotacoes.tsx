4cdf0ae242ecb307ff8e82f46a573ae57084174b
Tiago Trindade <tiagotrindade03@gmail.com>
2024-05-17 22:11:58 -0300

    data forge

--- src/pages/Rotacoes/Rotacoes.tsx
+++ src/pages/Rotacoes/Rotacoes.tsx
@@ -1,44 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { parseCsv } from '../../utils/csvUtils';  
-import { Rotacao } from '../../types/csvTypes';  
+import { DataFrame } from 'data-forge';
+import { parseCsv } from '../../utils/csvUtils';
+import { Rotacao } from '../../types/csvTypes';
 
 import './Rotacoes.css';
 import BackgroundTemplate from '../../components/Background Template/BackgroundTemplate';
 import RotacaoButton from '../../components/Rotacoes/RotacaoButton';
 
 const Rotacoes: React.FC = () => {
-    const [periodos, setPeriodos] = useState<string[]>([]); // Lista de periodos unicos
-    const [allData, setAllData] = useState<Rotacao[]>([]);  // Todos os dados da tabela Rotacao
+    const [periodos, setPeriodos] = useState<string[]>([]);
+    const [allData, setAllData] = useState<Rotacao[]>([]);
 
     useEffect(() => {
         parseCsv<Rotacao>('/data/rotacao.csv').then(data => {
             if (data) {
                 setAllData(data);
+                // Creating a DataFrame from the data
+                const df = new DataFrame(data);
+                // Extracting unique periods and sorting
+                const uniquePeriodos = df.deflate(row => row.periodo_rot)
+                                         .distinct()
+                                         .toArray()
+                                         .sort((a, b) => {
+                                             const [yearA, halfA] = a.split('_');
+                                             const [yearB, halfB] = b.split('_');
+
+                                             if (Number(yearA) !== Number(yearB)) {
+                                                 return Number(yearA) - Number(yearB);
+                                             }
+
+                                             return Number(halfA) - Number(halfB);
+                                         });
+                setPeriodos(uniquePeriodos);
             }
         });
     }, []);
 
-    useEffect(() => {
-        if (allData.length > 0) {
-            const uniquePeriodos = Array.from(new Set(allData.map(item => item.periodo_rot)))
-                .sort((a, b) => {
-                    const [yearA, halfA] = a.split('_');
-                    const [yearB, halfB] = b.split('_');
-
-                    if (Number(yearA) !== Number(yearB)) {
-                        return Number(yearA) - Number(yearB);
-                    }
-
-                    return Number(halfA) - Number(halfB);
-                });
-        setPeriodos(uniquePeriodos);
-        }
-    }, [allData]);  
-
     const transformPeriodo = (periodo: string): string => {
         const [year, half] = periodo.split('_');
-        const halfString = half === '1' ? '1' : '2';
-        return `20${year}.${halfString}`;
+        return `20${year}.${half === '1' ? '1' : '2'}`;
     };
 
     return (
